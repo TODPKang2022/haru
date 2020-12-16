4c4120fc83efede685879188f7e7ae7bd5129911
wangcho1999 <eastsky21@gmail.com>
2020-12-16 22:59:08 +0900

    settings

--- fastgram/settings/base.py
+++ fastgram/settings/base.py
@@ -14,8 +14,8 @@ import os
 import json
 from django.core.exceptions import ImproperlyConfigured
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
